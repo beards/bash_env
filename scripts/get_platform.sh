e497d7b6d31870c3f342ae1d7095d6cb5e2a329c
simon <simon_hsiung@trend.com.tw>
2012-09-13 16:32:42 +0800

    update git env setup script

---
+++ scripts/get_platform.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+lowercase() {
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+OS=`lowercase \`uname\``
+if [ "{$OS}" == "windowsnt" ]; then
+    OS=windows
+elif [ "{$OS}" == "darwin" ]; then
+    OS=mac
+fi
+
+export $OS
+
--- scripts/set_git.sh
+++ scripts/set_git.sh
@@ -7,11 +7,23 @@ git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.log1 log --pretty=oneline
 
+git config core.editor vim
+
+#
 # for git hub
-git_cmd_path=`which git`
-git_cmd_dir=`dirname $git_cmd_path`
-wget http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
-chmod u+x git-credential-osxkeychain
-sudo mv git-credential-osxkeychain $git_cmd_dir
-git config --global credential.helper osxkeychain
+#
+. ./get_platform.sh
+
+case $OS in
+    "linux" )
+        # Set git to use the credential memory cache that timeout after 1 hour (setting is in seconds)
+        git config --global credential.helper 'cache --timeout=3600'
+        ;;
+    "mac" )
+        curl -s -O http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
+        chmod +x git-credential-osxkeychain
+        sudo mv git-credential-osxkeychain `dirname \`which git\``
+        git config --global credential.helper osxkeychain
+        ;;
+esac
 

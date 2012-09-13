10ba5c636ba8aa49c925fcc3d603bcd25a96fc47
bear <bear.darkshine@gmail.com>
2012-09-13 16:44:20 +0800

    fix syntax bug in set_git.sh

--- scripts/set_git.sh
+++ scripts/set_git.sh
@@ -5,7 +5,7 @@ git config --global color.ui true
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.co checkout
-git config --global alias.log1 log --pretty=oneline
+git config --global alias.log1 'log --pretty=oneline'
 
 git config core.editor vim
 

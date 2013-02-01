e854b93a8fd9ea909ad747c619bc393547212f92
bear <bear.darkshine@gmail.com>
2013-02-01 20:05:56 +0800

    add git sub update in make_env

--- scripts/make_env.sh
+++ scripts/make_env.sh
@@ -2,6 +2,11 @@
 
 BASH_ENV_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd ../  && pwd )"
 
+# update scripts
+cd $BASH_ENV_DIR
+git submodule init
+git submodule update
+
 # bashrc
 cd ~
 mv .bashrc .bashrc.ori &> /dev/null
@@ -9,6 +14,7 @@ ln -s $BASH_ENV_DIR/.bashrc
 ln -s $BASH_ENV_DIR/scripts
 
 # profile for mac
+cd ~
 source $BASH_ENV_DIR/scripts/get_platform.sh
 if [ "$OS" == "mac" ]; then
     mv .profile .profile.ori &> /dev/null
@@ -16,10 +22,17 @@ if [ "$OS" == "mac" ]; then
 fi
 
 # screenrc
+cd ~
 mv .screenrc .screenrc.ori &> /dev/null
 ln -s $BASH_ENV_DIR/.screenrc
 
+# update vim modules
+cd $BASH_ENV_DIR/vimrc/
+git submodule init
+git submodule update
+
 # vimrc
+cd ~
 mv .vimrc .vimrc.ori &> /dev/null
 ln -s $BASH_ENV_DIR/vimrc/ .vim
 ln -s .vim/.vimrc

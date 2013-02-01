e1f6556e5caf06d7afe5b8912e5c3ebb8856a2d9
bear <bear.darkshine@gmail.com>
2013-02-01 19:51:16 +0800

    add screenrc in make_env

--- scripts/make_env.sh
+++ scripts/make_env.sh
@@ -4,12 +4,14 @@
 BASH_ENV_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd ../  && pwd )"
 
 cd ~
-mv .bashrc .bashrc.ori
+mv .bashrc .bashrc.ori &> /dev/null
 ln -s $BASH_ENV_DIR/.bashrc
 ln -s $BASH_ENV_DIR/scripts
+mv .screenrc .screenrc.ori &> /dev/null
+ln -s $BASH_ENV_DIR/.screenrc
 
 source $BASH_ENV_DIR/scripts/get_platform.sh
 if [ "$OS" == "mac" ]; then
-    mv .profile .profile.ori
+    mv .profile .profile.ori &> /dev/null
     ln -s $BASH_ENV_DIR/.profile
 fi

1d4ed8db5ad6236acafa22f1c57c44b69531468e
bear <bear.darkshine@gmail.com>
2013-02-01 19:58:25 +0800

    add vimrc

--- scripts/make_env.sh
+++ scripts/make_env.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-
 BASH_ENV_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd ../  && pwd )"
 
+# bashrc
 cd ~
 mv .bashrc .bashrc.ori &> /dev/null
 ln -s $BASH_ENV_DIR/.bashrc
 ln -s $BASH_ENV_DIR/scripts
-mv .screenrc .screenrc.ori &> /dev/null
-ln -s $BASH_ENV_DIR/.screenrc
 
+# profile for mac
 source $BASH_ENV_DIR/scripts/get_platform.sh
 if [ "$OS" == "mac" ]; then
     mv .profile .profile.ori &> /dev/null
     ln -s $BASH_ENV_DIR/.profile
 fi
+
+# screenrc
+mv .screenrc .screenrc.ori &> /dev/null
+ln -s $BASH_ENV_DIR/.screenrc
+
+# vimrc
+mv .vimrc .vimrc.ori &> /dev/null
+ln -s $BASH_ENV_DIR/vimrc/ .vim
+ln -s .vim/.vimrc

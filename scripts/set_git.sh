3df2bfbedd1d141b1ad4a654b2b790caee4c0af8
bear <bear.darkshine@gmail.com>
2013-02-06 19:25:43 +0800

    set push.default to current for git 1.7

--- scripts/set_git.sh
+++ scripts/set_git.sh
@@ -8,6 +8,6 @@ git config --global alias.co checkout
 git config --global alias.log1 'log --pretty=oneline'
 git config --global alias.sub submodule
 
-git config --global push.default simple
+git config --global push.default current
 
 git config --global core.editor vim

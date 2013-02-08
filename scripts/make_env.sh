ded140f0d8713174d68b8b6ca9b9df2b927f9ee0
bear <bear.darkshine@gmail.com>
2013-02-08 23:59:12 +0800

    install packages first and refine status outputs

--- scripts/make_env.sh
+++ scripts/make_env.sh
@@ -1,64 +1,67 @@
 #!/bin/bash
 
+SCRIPT_NAME=${BASH_SOURCE[0]}
 BASH_ENV_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd ../  && pwd )"
+DATE=$(date -d "today" +"%Y%m%d%H%M")
 
-# update scripts
-echo -e "# update scripts\n#"
+echo -e "#"
+echo -e "# $SCRIPT_NAME: update scripts"
+echo -e "#"
 cd $BASH_ENV_DIR
 git submodule init
 git submodule update
 
-# bashrc
-echo -e "# set bashrc\n#"
-cd ~
-mv .bashrc .bashrc.ori &> /dev/null
-ln -s $BASH_ENV_DIR/.bashrc
-ln -s $BASH_ENV_DIR/scripts
-source .bashrc
-
-# screenrc
-echo -e "# set screenrc\n#"
-cd ~
-mv .screenrc .screenrc.ori &> /dev/null
-ln -s $BASH_ENV_DIR/.screenrc
-
-# profile for mac
-cd ~
+echo -e "#"
+echo -e "# $SCRIPT_NAME: install necessary packages"
+echo -e "#"
 source $BASH_ENV_DIR/scripts/get_platform.sh
-if [ "$OS" == "mac" ]; then
-    echo -e "# set profile\n#"
-    mv .profile .profile.ori &> /dev/null
-    ln -s $BASH_ENV_DIR/.profile
-    source .profile
-fi
-
-# install homebrew on mac
-if [ "$OS" == "mac" ]; then
-    echo -e "# install homebrew\n#"
-    ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
-    brew update
-fi
-
-# install packages on linux
 if [ "$OS" == "debian" ]; then
-    echo -e "# install necessary packages\n#"
-    sudo apt-get install gcc python-dev
-    sudo apt-get install screen
-    sudo apt-get install exuberant-ctags
-    sudo apt-get install ack-grep
-    sudo apt-get install python-pip
-    sudo pip install flake8
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
     sudo $BASH_ENV_DIR/vimrc/build_vim_ubuntu.sh
+    sudo apt-get install -y screen ack-grep
 elif [ "$OS" == "redhat" ]; then
-    echo -e "# install necessary packages\n#"
     sudo yum install gcc python-devel
     sudo yum install screen
     sudo yum install ctags
     sudo yum install ack
     sudo yum install python-pip
     sudo pip install flake8
+elif [ "$OS" == "mac" ]; then
+    ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+    brew update
+    brew install macvim
 fi
 
-# git setting
-echo -e "# set git configs\n#"
+echo -e "#"
+echo -e "# $SCRIPT_NAME: set bashrc, screenrc"
+echo -e "#"
+cd ~
+mv .bashrc .bashrc.bak.$DATE &> /dev/null
+ln -s $BASH_ENV_DIR/.bashrc
+mv .screenrc .screenrc.bak.$DATE &> /dev/null
+ln -s $BASH_ENV_DIR/.screenrc
+mv scripts scripts.bak.$DATE &> /dev/null
+ln -s $BASH_ENV_DIR/scripts
+source .bashrc
+
+# profile for mac
+source $BASH_ENV_DIR/scripts/get_platform.sh
+if [ "$OS" == "mac" ]; then
+    echo -e "#"
+    echo -e "# $SCRIPT_NAME: set profile"
+    echo -e "#"
+    cd ~
+    mv .profile .profile.bak.$DATE &> /dev/null
+    ln -s $BASH_ENV_DIR/.profile
+    source .profile
+fi
+
+echo -e "#"
+echo -e "# $SCRIPT_NAME: set git configs"
+echo -e "#"
 $BASH_ENV_DIR/scripts/set_git.sh
+
+echo -e "#"
+echo -e "# $SCRIPT_NAME: done !"
+echo -e "#"

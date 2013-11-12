47e68d4d313d1530f504a41a794e0fb08c51cfb0
simon <simon_hsiung@trend.com.tw>
2013-11-12 14:03:39 +0800

    update build_clang.sh

--- scripts/build_clang.sh
+++ scripts/build_clang.sh
@@ -15,27 +15,47 @@ sudo apt-get install -y subversion g++ cmake build-essential libffi-dev
 echo -e "#"
 echo -e "# $SCRIPT_NAME: checkout llvm"
 echo -e "#"
-mkdir -p $SRC_DIR
-cd $SRC_DIR
-svn co http://llvm.org/svn/llvm-project/llvm/trunk .
+if [ ! -e $SRC_DIR ]; then
+    mkdir -p $SRC_DIR
+    cd $SRC_DIR
+    svn co http://llvm.org/svn/llvm-project/llvm/trunk .
+else
+    cd $SRC_DIR
+    svn update
+fi
 
 echo -e "#"
 echo -e "# $SCRIPT_NAME: checkout clang"
 echo -e "#"
 cd $SRC_DIR/tools
-svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+if [ ! -e "clang" ]; then
+    svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+else
+    cd clang
+    svn update
+fi
 
 echo -e "#"
 echo -e "# $SCRIPT_NAME: checkout extra clang tools"
 echo -e "#"
 cd $SRC_DIR/tools/clang/tools
-svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+if [ ! -e "extra" ]; then
+    svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+else
+    cd extra
+    svn update
+fi
 
 echo -e "#"
 echo -e "# $SCRIPT_NAME: checkout compiler-rt"
 echo -e "#"
 cd $SRC_DIR/projects
-svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+if [ ! -e "compiler-rt" ]; then
+    svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+else
+    cd compiler-rt
+    svn update
+fi
 
 echo -e "#"
 echo -e "# $SCRIPT_NAME: config build info"
